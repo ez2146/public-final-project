9968ca5600fc4c394ab2ed8ec350a35a9259b7ec
ethanzzheng <ethanzzheng@gmail.com>
2024-03-12 23:32:01 -0400

    Update db.mjs

---
+++ db.mjs
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    message: {type: String, required: true}
+  }); 
+  
+  
+  const User = mongoose.model('User', userSchema);
+
+  export {userSchema};
