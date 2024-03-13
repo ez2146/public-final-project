6fa5baeca2a90a21e9be7b292527ffdc6066d3ab
ethanzzheng <ethanzzheng@gmail.com>
2024-03-12 23:32:40 -0400

    added

--- app.mjs
+++ app.mjs
@@ -6,4 +6,16 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/*
+import mongoose from 'mongoose'
+//mongoose.connect(process.env.DSN ?? 'mongodb://localhost/class13')
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String, // don't store password in plain text
+  posts: [{type: mongoose.Types.ObjectId, ref: 'Post'}]
+})
+*/
+
+
 app.listen(process.env.PORT || 3000);
--- db.mjs
+++ db.mjs
@@ -9,4 +9,4 @@ const userSchema = new Schema({
   
   const User = mongoose.model('User', userSchema);
 
-  export {userSchema};
+  export {User};
